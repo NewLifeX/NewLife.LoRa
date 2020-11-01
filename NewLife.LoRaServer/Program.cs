a9864f3e4d20d3af6cd47ca3fa37479a311157c9
大石头 <nnhy@vip.qq.com>
2020-11-01 13:03:08 +0800

    更新基础组件

--- NewLife.LoRaServer/Program.cs
+++ NewLife.LoRaServer/Program.cs
@@ -14,9 +14,9 @@ namespace NewLife.LORAServer
 {
     class Program
     {
-        static void Main(String[] args) => new MyService().Main();
+        static void Main(String[] args) => new MyService().Main(args);
 
-        class MyService : AgentServiceBase<MyService>
+        class MyService : ServiceBase
         {
             public MyService()
             {
